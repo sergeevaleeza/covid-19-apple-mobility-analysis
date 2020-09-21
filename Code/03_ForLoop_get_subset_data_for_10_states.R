9dd28d772484c4c59505ba9b169bafe6833ec283
Leeza Sergeeva <sergeevaleeza@gmail.com>
2020-09-20 20:50:27 -0700

    updated script 01_ to give data for 10 states

---
+++ Code/03_ForLoop_get_subset_data_for_10_states.R
@@ -0,0 +1,27 @@
+# script to read Apple mobility data from Covid-19
+# subset data to include rows from any state
+# and extract to csv file
+
+# Leeza Sergeeva
+# esergeeva@dons.usfca.edu
+# 2020-09-20
+
+# use a function to subset any US state from original dataset
+# create output .csv file containing original data name + state name
+# use function for a list of 10 states
+
+#load functions
+source("Code/Functions/state_mobility_data.R")
+
+# Top 10 states by population were chosen
+states_list_of_ten <- list("California", "Texas", "Florida", "New York",
+                           "Pennsylvania", "Illinois", "Ohio", "Georgia",
+                           "North Carolina", "Michigan")
+
+# for loop to iterate through list of 10 states.
+for (state_to_choose in states_list_of_ten) {
+  state_mobility_data(
+    input_file_name = "Data/Raw/applemobilitytrends-2020-09-18.csv",
+    state = state_to_choose
+  )
+}
