33d8f05552bd332858c77e02e4981619fe463f84
Leeza Sergeeva <sergeevaleeza@gmail.com>
2020-10-24 16:44:27 -0700

    Update README file, add defensive function to bash script

--- analyze_covid_pipeline_run.sh
+++ analyze_covid_pipeline_run.sh
@@ -4,11 +4,20 @@
 # using some parameters passed in from the shell
 # This script expects two command line parameters,
 # the first parameter is a US state,
-# the second parameter is raw data to process.
+# the second parameter is raw mobility data to process
+# downloaded from Apple COVID mobility data website.
 
 # Elizaveta Sergeeva
 # October 24, 2020
 # esergeeva@usfca.edu
+
+if [ $# -eq 0 ]
+then 
+  echo "To run this script, supply two arguments:"
+  echo "The first is the name of the US state to be analyzed."
+  echo "The socond is the path to raw mobility data csv file."
+  exit 1
+fi
 
 Rscript -e "rmarkdown::render('Analysis.Rmd', \
             params = list(state = '$1', \
