d4adaacac46c7efa82a7ebb2c8f69387eb4345cc
Leeza Sergeeva <sergeevaleeza@gmail.com>
2020-11-23 21:47:50 +0000

    Add bash command script

---
+++ Code/.profile
@@ -0,0 +1,5 @@
+export BLASTDB=/blast-db
+git config --global core.editor nano
+# comment out this, because it was chnaged with cdsh on 10/24/2020 LS
+# /usr/bin/bash
+
---
+++ Code/process_sequences.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is bash script to analyze sequence data for SARS-CoV2 virus
+# provided by NCBI website.
+
+# Leeza Sergeeva
+# October 24, 2020
+# esergeeva@usfca.edu
+
+
